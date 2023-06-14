dc2f16df5a593846a9577a2b66538fcff643b763
lamb1303 <lamb1303@outlook.com>
2023-06-14 12:00:55 -0600

    fix :adhesive_bandage: Turno Page updated

---
+++ src/helpers/stationEnum.js
@@ -0,0 +1,16 @@
+const StationEnum = {
+  reg: "Registro",
+  nur: "Enfermería",
+  doc: "Doctor",
+  pt: "Terapia Física",
+  ped: "Pediatría",
+  nut: "Nutrición",
+  obs: "Observación",
+  pha: "Farmacia",
+  lab: "Laboratorio",
+  pra: "Sala de Procedimientos A",
+  fin: "Finalizar",
+  pay: "Pago",
+};
+
+export default StationEnum;
--- src/pages/Turno.js
+++ src/pages/Turno.js
@@ -1,14 +1,101 @@
 import React, { useEffect, useState } from "react";
-import { Table, Image, Alert, Divider, Radio } from "antd";
+import { Table, Image, Alert, Divider, Typography, Space } from "antd";
 import { firestore } from "./../helpers/firebaseConfig";
 import { useHideMenu } from "../hooks/useHideMenu";
+import StationEnum from "../helpers/stationEnum";
+const { Title } = Typography;
 
 export const Turno = () => {
   useHideMenu(true);
   const [data, setData] = useState([]);
-  const [status, setStatus] = useState(1);
 
-  console.log(data);
+  const renderStatusIcon = (status) => {
+    switch (status) {
+      case "pending":
+        return (
+          <Image
+            src={require("../img/not_planned.svg")}
+            width={20}
+            height={80}
+          />
+        );
+      case "in_process":
+        return (
+          <Image src={require("../img/waiting.svg")} width={20} height={80} />
+        );
+      case "waiting":
+        return (
+          <Image src={require("../img/waiting.svg")} width={20} height={80} />
+        );
+      case "pay":
+        return <Image src={require("../img/pay.svg")} width={20} height={80} />;
+      case "complete":
+        return (
+          <Image src={require("../img/complete.svg")} width={20} height={80} />
+        );
+      default:
+        return (
+          <Image
+            src={require("../img/not_planned.svg")}
+            width={20}
+            height={80}
+          />
+        );
+    }
+  };
+
+  const generateTableData = (extractedPlanOfCare) => {
+    const uniqueStations = {};
+    extractedPlanOfCare.forEach((item) => {
+      // eslint-disable-next-line no-unused-expressions
+      item.plan_of_care?.forEach((plan) => {
+        if (!uniqueStations[plan.station]) {
+          uniqueStations[plan.station] = {
+            dataIndex: plan.station,
+            key: plan.station,
+            title: StationEnum[plan.station],
+            render: (status) => renderStatusIcon(status),
+            width: 100,
+            align: "center",
+          };
+        }
+      });
+    });
+    const columns = [
+      {
+        title: "Patient",
+        dataIndex: "patient_name",
+        key: "patient",
+        width: 100,
+        fixed: "left",
+      },
+      ...Object.values(uniqueStations),
+      {
+        title: "Tiempo de espera",
+        dataIndex: "",
+        key: "",
+        width: 100,
+        fixed: "right",
+      },
+    ];
+
+    const dataSource = extractedPlanOfCare.map((item) => {
+      const stations = {};
+      item.plan_of_care.forEach((plan) => {
+        stations[plan.station] = plan.status;
+      });
+      return {
+        pt_no: item.pt_no,
+        patient_name: item.patient_name,
+        ...stations,
+      };
+    });
+
+    return { columns, dataSource };
+  };
+
+  const { columns, dataSource } = generateTableData(data);
+
   useEffect(() => {
     let isMounted = true;
     const fetchData = async () => {
@@ -38,115 +125,6 @@ export const Turno = () => {
     fetchData();
   }, []);
 
-  const changeStatus = (e) => {
-    console.log('radio checked', e.target.value);
-    setStatus(e.target.value);
-  };
-  
-
-  const columns = [
-    {
-      title: "Paciente",
-      dataIndex: "paciente", // Nuevo campo: Paciente
-      key: "11",
-      width: 110,
-    },
-    {
-      title: "Médico General",
-      dataIndex: "med",
-      key: "1",
-      width: 40,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Enfermería",
-      dataIndex: "enf",
-      key: "2",
-      width: 50,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Laboratorio",
-      dataIndex: "lab",
-      key: "3",
-      width: 50,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Fisiólogo",
-      dataIndex: "fis",
-      key: "4",
-      width: 40,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Pediatra",
-      dataIndex: "ped",
-      key: "5",
-      width: 40,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Nutriólogo",
-      dataIndex: "nut",
-      key: "6",
-      width: 40,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Obstétrico",
-      dataIndex: "obs",
-      key: "7",
-      width: 40,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Neurólogo",
-      dataIndex: "neu",
-      key: "9",
-      width: 40,
-      render: () => (
-        <div className="center-cell">
-          <Radio onChange={changeStatus} value={1} size="large"></Radio>{" "}
-        </div>
-      ),
-    },
-    {
-      title: "Tiempo",
-      dataIndex: "tiempo", // Nuevo campo: Timestamp
-      key: "14",
-      width: 50,
-    },
-  ];
-
   const getRowClassName = (record, index) => {
     return index % 2 === 0 ? "even-row" : "odd-row";
   };
@@ -154,51 +132,57 @@ export const Turno = () => {
   return (
     <>
       <Alert
-        message="Información del Estatus del Paciente"
+        message="Información del Estatus del Paciente "
         description={
-          <>
-            <p>
-              <Image
-                src={require("../img/not_planned.svg")}
-                width={15}
-                height={10}
-              />
-              {"Procesando información del paciente"}
-            </p>
-            <p>
+          <Space
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Space>
               <Image
                 src={require("../img/waiting.svg")}
-                width={15}
-                height={10}
+                width={20}
+                height={80}
               />
-              {"Paciente en espera"}
-            </p>
-            <p>
+              <Title level={5}>{"Paciente en espera "}</Title>
+            </Space>
+            <Divider />
+            <Space>
               <Image
                 src={require("../img/in_process.svg")}
-                width={15}
-                height={10}
+                width={20}
+                height={80}
               />
-              {"Paciente siendo atendido"}
-            </p>
-            <p>
+              <Title level={5}>{"Paciente siendo atendido "}</Title>
+            </Space>
+            <Divider />
+            <Space>
+              <Image src={require("../img/pay.svg")} width={20} height={80} />
+              <Title level={5}> {"Paciente realizó el pago "}</Title>
+            </Space>
+            <Divider />
+            <Space>
               <Image
                 src={require("../img/complete.svg")}
-                width={15}
-                height={10}
+                width={20}
+                height={80}
               />
-              {"El paciente completó su visita"}
-            </p>
-          </>
+              <Title level={5}> {"Paciente completó su visita "}</Title>
+            </Space>
+          </Space>
         }
         type="info"
         showIcon
       />
+
       <Divider />
       <Table
-        rowKey={"paciente"}
+        rowKey={"pt_no"}
         columns={columns}
-        dataSource={data.some((d) => d === undefined) ? [] : data}
+        dataSource={data.some((d) => d === undefined) ? [] : dataSource}
         scroll={{ x: 1500, y: 1500 }}
         sticky
         offsetScroll={3}
